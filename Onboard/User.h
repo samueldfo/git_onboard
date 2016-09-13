1d7f388cb254266d7b73c99b741d98ab1d27a0cb
samueldfo <samueldfo@gmail.com>
2016-09-13 12:34:35 -0300

    moved  directory location

---
+++ Onboard/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  Onboard
+//
+//  Created by Taqtile on 13/09/16.
+//  Copyright © 2016 Taqtile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ Onboard/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Onboard
+//
+//  Created by Taqtile on 13/09/16.
+//  Copyright © 2016 Taqtile. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
